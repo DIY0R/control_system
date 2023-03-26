d12c16a5176fe8108f91c54282f1cdaff2833286
diyor <sabirovdiyorkind@gmail.com>
2023-03-26 18:25:24 +0500

    set up commit linter

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'refactor', 'chore', 'docs', 'test', 'ci'],
+    ],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+  },
 };
