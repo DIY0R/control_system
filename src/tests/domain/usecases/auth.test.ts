9873c970269df4863811851d18ab88cacdd544f8
diyor <sabirovdiyorkind@gmail.com>
2023-03-26 16:21:55 +0500

    test: update auth.test.ts

--- src/tests/domain/usecases/auth.test.ts
+++ src/tests/domain/usecases/auth.test.ts
@@ -1,30 +1,16 @@
-import { UserInfo } from 'os';
 import { AuthError } from '../../../domain/description_objects/error/auth.error';
 import { UserLoginDto } from '../../../domain/description_objects/user/user.dto';
 import { User } from '../../../domain/entities/user/user';
-import { UserRepo } from '../../../domain/repositories/user.auth';
 import { AuthUseCase } from '../../../domain/usecases/user/auth.usecase';
 
 describe('AuthTests login', () => {
-  //   const errorGenerate: UserRepo = {
-  //     create,
-  //   };
   const AuthErrorImpl: AuthError = {
     loginError: { error: 'forbidden', code: 403 },
   };
   const baseHesh = {
     base: jest.fn((a: number, b: number) => (a == b ? true : false)),
   };
-  const authUseCase = new AuthUseCase(
-    {} as any,
-    baseHesh as any,
-    AuthErrorImpl
-  );
-
-  afterEach(() => {
-    baseHesh.base.mockClear();
-    checkNick.mockClear();
-  });
+  let loginData: UserLoginDto;
   const getOneUser: User = {
     id: 1,
     description: ['hr', 'admin'],
@@ -35,26 +21,36 @@ describe('AuthTests login', () => {
     photo: 'https://presto/hi.jpg',
     role: ['HR', 'MIDDLE'],
   };
-  const checkNick = jest
-    .spyOn(authUseCase as any, 'checkNick')
-    .mockImplementation(
-      async (nick): Promise<User | null> =>
-        nick == getOneUser.nick ? getOneUser : null
-    );
+  let userRepoMock = {
+    findOneByNick: jest.fn(async (nick) =>
+      getOneUser.nick == nick ? getOneUser : null
+    ),
+  };
+
+  const authUseCase = new AuthUseCase(
+    userRepoMock as any,
+    baseHesh as any,
+    AuthErrorImpl
+  );
+  beforeEach(() => {
+    loginData = {} as UserLoginDto;
+  });
+  afterEach(() => {
+    userRepoMock.findOneByNick.mockClear();
+    baseHesh.base.mockClear();
+  });
 
   test('sucess', async () => {
-    const loginData: UserLoginDto = {
+    loginData = {
       nick: 'vista',
       password: '123',
     };
     expect(await authUseCase.login(loginData)).toEqual(getOneUser);
-
-    expect(checkNick).toBeCalledTimes(1);
     expect(baseHesh.base).toHaveBeenCalledTimes(1);
   });
 
   test('fall incorrect nick', async () => {
-    const loginData: UserLoginDto = {
+    loginData = {
       nick: 'vista_error',
       password: '123',
     };
@@ -62,12 +58,12 @@ describe('AuthTests login', () => {
     expect(await authUseCase.login(loginData)).toEqual(
       AuthErrorImpl.loginError
     );
-    console.log(checkNick.mock.results);
-    expect(await checkNick.mock.results[0].value).toEqual(null);
+    expect(await userRepoMock.findOneByNick.mock.results[0].value).toBeNull();
+    expect(baseHesh.base).toHaveBeenCalledTimes(0);
   });
 
   test('fall incorrect password', async () => {
-    const loginData: UserLoginDto = {
+    loginData = {
       nick: 'vista',
       password: '123_2',
     };
@@ -75,7 +71,7 @@ describe('AuthTests login', () => {
     expect(await authUseCase.login(loginData)).toEqual(
       AuthErrorImpl.loginError
     );
-    expect(await checkNick.mock.results[0].value).toEqual(getOneUser);
+    expect(await userRepoMock.findOneByNick).toHaveBeenCalledTimes(1);
     expect(await baseHesh.base.mock.results[0].value).toEqual(false);
   });
 });
