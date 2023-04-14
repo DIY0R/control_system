19c0ed9c4c1dd98d0bf6fb94731386b0c338ca2c
diyor <sabirovdiyorkind@gmail.com>
2023-04-14 15:53:26 +0500

    feat: added user registration logic, including checking for uniqueness of data

---
+++ src/domain/usecases/user/registration.usecase.ts
@@ -0,0 +1,46 @@
+import { AuthError } from '../../description_objects/error/auth.error';
+import {
+  UserLoginDto,
+  UserRegistrationDto,
+} from '../../description_objects/user/user.dto';
+import { User } from '../../entities/user/user';
+import { UserRepository } from '../../repositories/user.auth';
+import {
+  checkEmailStrength,
+  checkPasswordStrength,
+} from '../../utils/authUtils';
+
+export class AuthUseCase {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly HashGenerator: any,
+    private readonly errorObject: AuthError
+  ) {}
+
+  async registration(
+    userRegistrationDto: UserRegistrationDto
+  ): Promise<User | Error> {
+    const { email, name, nick, password, role } = userRegistrationDto;
+
+    if (!checkEmailStrength(email) || !checkPasswordStrength(password))
+      throw new Error(this.errorObject.registrationError);
+
+    const userByNick = await this.userRepository.findOneByNick(nick);
+    const userByEmail = await this.userRepository.findOneByEmail(email);
+
+    if (!!userByNick || !!userByEmail)
+      throw new Error(this.errorObject.registrationError);
+
+    const hashPassword = await this.HashGenerator.hash(password, 5);
+    const newUser = await this.userRepository.create({
+      email,
+      name,
+      nick,
+      password: hashPassword,
+      role,
+      description: [],
+      photo: '',
+    });
+    return newUser;
+  }
+}
