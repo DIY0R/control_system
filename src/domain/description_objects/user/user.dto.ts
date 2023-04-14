d0242411222c23dcb40f2ca1d64251cd80b80b2e
diyor <sabirovdiyorkind@gmail.com>
2023-04-14 15:59:53 +0500

    feat: create registration dto

--- src/domain/description_objects/user/user.dto.ts
+++ src/domain/description_objects/user/user.dto.ts
@@ -1,4 +1,10 @@
+import { User } from '../../entities/user/user';
+
 export interface UserLoginDto {
   nick: string;
   password: string;
 }
+export type UserRegistrationDto = Pick<
+  User,
+  'name' | 'nick' | 'email' | 'password' | 'role'
+>;
