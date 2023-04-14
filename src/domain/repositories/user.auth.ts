0fc5d8b49be76a521efb97524efe800bb3b991e1
diyor <sabirovdiyorkind@gmail.com>
2023-04-14 16:12:20 +0500

    refactor: changing logic in auth

--- src/domain/repositories/user.auth.ts
+++ src/domain/repositories/user.auth.ts
@@ -1,6 +1,6 @@
 import { User } from '../entities/user/user';
 
-export abstract class UserRepo {
+export abstract class UserRepository {
   abstract getAll(page: number, lim: number): Promise<User[]>;
   abstract findOneByNick(nick: string): Promise<User>;
   abstract findOneByEmail(email: string): Promise<User>;
